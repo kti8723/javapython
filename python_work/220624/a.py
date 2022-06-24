6630afe8c658969d72025d476fac664562b8f61e
kti8723 <kti8923@naver.com>
2022-06-24 14:00:55 +0900

    220624

---
+++ python_work/220624/a.py
@@ -0,0 +1,11 @@
+def greet():
+    print("한글")
+    print("파이썬의 세계로 오신것을 환영합니다.")
+    a=10
+    b=20
+    print(a)
+    print(b)
+
+greet()
+print("aa")
+greet()
---
+++ python_work/220624/b.py
@@ -0,0 +1,6 @@
+def aaa(name, age):
+    print(name, ' hello'L)
+    print(age)
+
+    aaa('john',30)
+    aaa('kelly',23)
